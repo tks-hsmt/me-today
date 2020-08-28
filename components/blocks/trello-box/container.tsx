10484dcda9a1b421c6ee98557fd14d22a09e7514
thashimoto <hashimoto.takeshi@ntc.co.jp>
2020-08-28 19:16:09 +0900

    データ取得中はローディングを表示するように修正

--- components/blocks/trello-box/container.tsx
+++ components/blocks/trello-box/container.tsx
@@ -3,7 +3,7 @@ import { useTrelloStore } from '../../../store';
 import TrelloBox from '.';
 
 const TrelloBoxContainer: React.FC = () => {
-  const { tasks, init, fetchAll, isAuthorized } = useTrelloStore();
+  const { tasks, init, fetchAll, isAuthorized, isFetching } = useTrelloStore();
   useEffect(() => {
     if (!isAuthorized) {
       init();
@@ -12,7 +12,7 @@ const TrelloBoxContainer: React.FC = () => {
     fetchAll().then(payload => console.log(payload)).catch(error => console.log(error));
   }, [fetchAll, isAuthorized]);
 
-  return <TrelloBox tasks={tasks} isAuthorized={isAuthorized} />;
+  return <TrelloBox tasks={tasks} isAuthorized={isAuthorized} isFetching={isFetching} />;
 }
 
 export default TrelloBoxContainer;
--- components/blocks/trello-box/index.tsx
+++ components/blocks/trello-box/index.tsx
@@ -2,6 +2,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import AppPaper from '../../elements/paper';
 import AppCard from '../../elements/card';
 import { TrelloTask } from '../../../models';
@@ -20,7 +21,8 @@ const TrelloBox: React.FC<Props> = props => {
     <Box display='flex' flexDirection='column' className={classes.root}>
       <Typography>Trello Tasks</Typography>
       <AppPaper className={classes.appPaper}>
-        {isAuthorized && tasks.map(task => (
+        {isAuthorized && isFetching && <Box display='flex' justifyContent='center' m={2}><CircularProgress color="secondary" /></Box>}
+        {isAuthorized && !isFetching && tasks.map(task => (
           <AppCard key={task.id} className={classes.card} title={task.name} subTitle={task.due} headerImage='trello-mark-blue.png'>
             <Link href={task.url} target='_blank' rel='noopener' color='inherit'>Trelloで開く</Link>
           </AppCard>
